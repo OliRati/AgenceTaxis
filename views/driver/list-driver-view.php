78b99301659cf80f3ad01ed347f5baea61ea9f92
Olivier RATIEVILLE <olivier.ratieville@gmail.com>
2025-12-17 13:45:32 +0100

    Add pico.css.min to assets/css and link to it

--- views/driver/list-driver-view.php
+++ views/driver/list-driver-view.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chauffeurs</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
+    <link rel="stylesheet" href="<?= WEB_ROOT . "/assets/css/pico.min.css" ?>">
 </head>
 
 <body>
